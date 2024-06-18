56b571d1c0541dd6d0aea5484d1295fd9e02b020
LealDev <lealmaster0111@gmail.com>
2024-06-18 16:17:43 -0300

    adicionando controller

---
+++ manager-backend/src/main/java/com/biblioteca/manager/HomeController.java
@@ -0,0 +1,16 @@
+package com.biblioteca.manager;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Hello, World!";
+    }
+}
